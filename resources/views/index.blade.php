d3e778895d136e177878db0e626cd8269ca8b466
Aaron Belz <belzaaron@gmail.com>
2022-02-05 22:33:59 -0500

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta property="og:title" content="{{ $joke }} | Belz.dev" />
+    <meta property="og:title" content="Probably the funniest joke ever!" />
     <meta property="og:url" content="{{ url('/') }}" />
 
     <title>HAHA!</title>
